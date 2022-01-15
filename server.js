67fc40a5f5283281f961fcc9a28c4b34c57f8b47
unknown <b07901015@ntu.edu.tw>
2022-01-15 14:25:09 +0800

    deploy on heroku

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ import router from './backend/src/routes/api.js'
 import bodyParser from 'body-parser';
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
