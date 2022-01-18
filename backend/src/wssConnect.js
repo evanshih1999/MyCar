6e33da9d2fb2781e5d22448507e19057eeb1fc7c
unknown <b07901015@ntu.edu.tw>
2022-01-18 21:26:35 +0800

    main

---
+++ backend/src/wssConnect.js
@@ -0,0 +1,25 @@
+import WebSocket from 'ws'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const wssConnect = (server) => {
+    const wss = new WebSocket.Server({ server })
+    
+    const sendFile = (buf) => {
+        wss.clients.forEach((client) => {
+            /*fs.readFile(__dirname+'/assets/sample.jpg', function(err, buf){
+                client.send(JSON.stringify({ image: true, buffer: buf.toString('base64') }))
+                console.log('file sent')
+            })*/
+            client.send(JSON.stringify({ image: true, buffer: buf.toString('base64') }))
+        });
+    }
+
+    return {wss, sendFile}
+}
+
+
+export default wssConnect
