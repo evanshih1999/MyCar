88e95714dc21db95258f0409bc3f1ac6fe4bb959
unknown <b07901015@ntu.edu.tw>
2022-01-20 22:59:03 +0800

    deploy on heroku

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const url = new URL("/", window.location.href);
 
 const instance = axios.create({
-  baseURL: `http://localhost:5000/`,
-  //baseURL: url.href
+  //baseURL: `http://localhost:5000/`,
+  baseURL: url.href
 });
 
 export default instance;
